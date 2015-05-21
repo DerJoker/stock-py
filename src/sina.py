e98595928bee902de49bb9632b28013deb73ac6f
Ya Feng <fengya09@gmail.com>
2015-05-21 21:57:52 +0800

    return string with "empty" value separated by same amount of ',' (32) if TimeOut

--- src/sina.py
+++ src/sina.py
@@ -3,7 +3,7 @@ import urllib2
 CODEC = 'gb2312'
 
 debug = True
-debug = False
+# debug = False
 
 class StockInfo:
     
@@ -21,21 +21,19 @@ class StockInfo:
             return urllib2.urlopen(url, timeout=10).read()
         except Exception,e:
             if (debug): print e
-            return ''
+            # return string with "empty" value separated by same amount of ',' (32) if TimeOut
+            return ',' * 32
     
     def parseResults(self):
-        resstr = self.read().decode(CODEC)
-        if (debug): print resstr
+        if (debug): print self.read().decode(CODEC)
+        res = self.read().decode(CODEC).split(',')
         
-        if resstr != '':
-            res = resstr.split(',')
-            company_name = res[0].split('"')[-1]
-            price_yesterday_close = res[1]
-            price_today_open = res[2]
-            price = res[3]
-            return (company_name, self.symbol, price)
-        
-        return None
+        company_name = res[0].split('"')[-1]
+        price_yesterday_close = res[1]
+        price_today_open = res[2]
+        price = res[3]
+        return (company_name, self.symbol, price)
+
 
 '''
 UnitTest
